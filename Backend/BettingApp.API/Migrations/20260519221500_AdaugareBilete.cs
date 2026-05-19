88cc10dc67625637628af7965dd6f347ec77f3ca
Radu Mihacea-Geger <radu.mihacea@gmail.com>
2026-05-20 01:20:15 +0300

    Am adaugat modelele Ticket si TicketMatch plus migratia bazei de date

---
+++ Backend/BettingApp.API/Migrations/20260519221500_AdaugareBilete.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BettingApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdaugareBilete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
